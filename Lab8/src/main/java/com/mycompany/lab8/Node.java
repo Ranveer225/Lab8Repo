85ae9bb82b08c121bfd706954c3541f0b385e3cc
Ranveer Singh PC <Ranveer Singh PC@RanveerSingh-PC.lan>
2023-04-02 12:36:26 -0400

    create Node class to move prime numbers

---
+++ Lab8/src/main/java/com/mycompany/lab8/Node.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab8;
+
+/**
+ *
+ * @author Ranveer Singh PC
+ */
+class Node {
+    public int data;
+    public Node next;
+    
+    public Node (int data){
+        this.data =data;
+        this.next=null;
+    }
+}
+
+class LinkedList {
+    private Node tail;
+    private Node head;
+  public LinkedList(){
+      tail =null;
+      head=null;
+  }
+}
